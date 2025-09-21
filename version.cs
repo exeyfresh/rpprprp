9d913c9c91eadc0cd81f0fd2ff394698e1864813
Denis <157739473+exeyfresh@users.noreply.github.com>
2025-09-21 18:42:18 +0300

    Добавил 3 версию

--- version.cs
+++ version.cs
@@ -2,19 +2,52 @@ using System;
 
 class Program
 {
+    enum TempUnit
+    {
+        Celsius = 1,
+        Fahrenheit,
+        Kelvin
+    }
+
     static void Main()
     {
-        Console.Write("Ââåäèòå òåìïåðàòóðó â Ôàðåíãåéòàõ: ");
-        string input = Console.ReadLine();
+        Console.WriteLine("Âûáåðèòå èñõîäíóþ åäèíèöó èçìåðåíèÿ:");
+        Console.WriteLine("1 - Öåëüñèé");
+        Console.WriteLine("2 - Ôàðåíãåéò");
+        Console.WriteLine("3 - Êåëüâèí");
+        Console.Write("Ââåäèòå íîìåð: ");
+        int fromUnit = int.Parse(Console.ReadLine());
 
-        if (double.TryParse(input, out double fahrenheit))
-        {
-            double celsius = (fahrenheit - 32) * 5 / 9;
-            Console.WriteLine($"Òåìïåðàòóðà â Öåëüñèÿõ: {celsius:F2}");
-        }
-        else
-        {
-            Console.WriteLine("Îøèáêà: ââåäåíî íåêîððåêòíîå çíà÷åíèå.");
-        }
+        Console.WriteLine("Âûáåðèòå êîíå÷íóþ åäèíèöó èçìåðåíèÿ:");
+        Console.WriteLine("1 - Öåëüñèé");
+        Console.WriteLine("2 - Ôàðåíãåéò");
+        Console.WriteLine("3 - Êåëüâèí");
+        Console.Write("Ââåäèòå íîìåð: ");
+        int toUnit = int.Parse(Console.ReadLine());
+
+        Console.Write("Ââåäèòå òåìïåðàòóðó: ");
+        double temp = double.Parse(Console.ReadLine());
+
+        double tempInCelsius = 0;
+
+        // Ïåðåâîäèì â Öåëüñèé
+        if (fromUnit == (int)TempUnit.Celsius)
+            tempInCelsius = temp;
+        else if (fromUnit == (int)TempUnit.Fahrenheit)
+            tempInCelsius = (temp - 32) * 5 / 9;
+        else if (fromUnit == (int)TempUnit.Kelvin)
+            tempInCelsius = temp - 273.15;
+
+        double result = 0;
+
+        // Ïåðåâîäèì èç Öåëüñèÿ â íóæíóþ øêàëó
+        if (toUnit == (int)TempUnit.Celsius)
+            result = tempInCelsius;
+        else if (toUnit == (int)TempUnit.Fahrenheit)
+            result = tempInCelsius * 9 / 5 + 32;
+        else if (toUnit == (int)TempUnit.Kelvin)
+            result = tempInCelsius + 273.15;
+
+        Console.WriteLine($"Ðåçóëüòàò: {result:F2}");
     }
 }
